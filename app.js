6d55d6ad3652679bab5eee75e82ddc2bd0a1bf59
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-07-29 01:14:04 +0530

    final product

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var inputBill = document.querySelector("#bill-amount");
 var btnBillNext = document.querySelector("#btn-bill-value");
 var inputCash = document.querySelector("#cash-amount");
 var btnCashNext = document.querySelector("#btn-cash-value");
-var notes= document.querySelectorAll("#notes");
+var notes= document.querySelector("#notes");
+var cashInput = document.querySelector("#cash-input");
+var changeOutput = document.querySelector("#change-output");
 
 const arrayNotes = [200,100,50,10,5,2,1];
 
@@ -10,28 +12,56 @@ btnBillNext.addEventListener("click", function clickHandler(){
     console.log(inputBill.value);
 
     if(inputBill.value >= 1){
+        btnBillNext.style.display = "none";
+        cashInput.style.display = "block";
         btnCashNext.addEventListener("click", function clickHandler(){
             console.log(inputCash.value);
         
             if(inputCash.value >= inputBill.value){
                 
+                changeOutput.style.display = "block";
+                btnCashNext.style.display = "none";
                 let noteCount = 0;
                 let returnAmount = inputCash.value - inputBill.value;
                 console.log(returnAmount);
 
-                for(let i=0;i<arrayNotes.length;i++){
+                // for(let i=0;i<arrayNotes.length;i++){
                     
+                //     if(returnAmount >= arrayNotes[i]){
+                        
+                //         for(returnAmount;returnAmount >= arrayNotes[i];){
+                //         returnAmount = returnAmount - arrayNotes[i];
+                    
+                //         console.log(arrayNotes[i]);
+                //         console.log(returnAmount);
+                //         document.getElementById("notes").innerHTML = arrayNotes[i] + noteCount++;
+                //         }
+                        
+                //     }
+                // }
+            
+
+                for(let i=0;i<arrayNotes.length;i++){
+                   
                     if(returnAmount >= arrayNotes[i]){
                         
-                        for(returnAmount;returnAmount >= arrayNotes[i];){
-                        returnAmount = returnAmount - arrayNotes[i];
-                        noteCount++
+                        let arrayValue = arrayNotes[i];
+                        console.log(arrayValue);
                         
-                        console.log(arrayNotes[i]);
-                        console.log(returnAmount);
+                        let remainder = Math.floor(returnAmount / arrayValue) ;
+                        console.log(remainder);
+
+                        var notesAndCount=document.createElement("H4");
+                var message=document.createTextNode(arrayValue + "  "+ "|" + " "+remainder );
+                notesAndCount.appendChild(message);
+                document.body.appendChild(notesAndCount);
+
+
+                        for(returnAmount;returnAmount >= arrayNotes[i];){
+                             returnAmount = returnAmount - arrayNotes[i];
                         }
-                        notes.innerHTML += noteCount;
                     }
+
                 }
 
             }
