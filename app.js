2aa8ba53f50ff90106d10d92ea55c75b8431379d
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-08-15 22:12:56 +0530

    refactor -change ifelse loop type string to number

--- app.js
+++ app.js
@@ -1,85 +1,66 @@
-var inputBill = document.querySelector("#bill-amount");
+let inputBill = document.querySelector("#bill-amount");
 var btnBillNext = document.querySelector("#btn-bill-value");
-var inputCash = document.querySelector("#cash-amount");
+let inputCash = document.querySelector("#cash-amount");
 var btnCashNext = document.querySelector("#btn-cash-value");
-var notes= document.querySelector("#notes");
+var notes = document.querySelector("#notes");
 var cashInput = document.querySelector("#cash-input");
 var changeOutput = document.querySelector("#change-output");
 
-const arrayNotes = [200,100,50,10,5,2,1];
 
-btnBillNext.addEventListener("click", function clickHandler(){
-    console.log(inputBill.value);
+const arrayNotes = [2000, 500, 100, 50, 10, 5, 2, 1];
 
-    if(inputBill.value >= 1){
+btnBillNext.addEventListener("click", function clickHandler() {
+
+    //check bill amount is not 0 or less than zero.
+    if (inputBill.value >= 1) {
         btnBillNext.style.display = "none";
         cashInput.style.display = "block";
-        btnCashNext.addEventListener("click", function clickHandler(){
-            console.log(inputCash.value);
-        
-            if(inputCash.value >= inputBill.value){
-                
+        btnCashNext.addEventListener("click", function clickHandler() {
+            let newInputBill = inputBill.value;
+            let newInputCash = inputCash.value;
+
+            // check cash amount is greater than bill amount
+            if (Number(newInputCash) < Number(newInputBill)) {
+
+                var newHeading = document.createElement("H3");
+                var message = document.createTextNode("Cash amount is less than Bill amount !");
+                newHeading.appendChild(message);
+                document.body.appendChild(newHeading);
+
+            } else {
                 changeOutput.style.display = "block";
                 btnCashNext.style.display = "none";
-                let noteCount = 0;
-                let returnAmount = inputCash.value - inputBill.value;
-                console.log(returnAmount);
+                let returnAmount = newInputCash - newInputBill;
 
-                // for(let i=0;i<arrayNotes.length;i++){
-                    
-                //     if(returnAmount >= arrayNotes[i]){
-                        
-                //         for(returnAmount;returnAmount >= arrayNotes[i];){
-                //         returnAmount = returnAmount - arrayNotes[i];
-                    
-                //         console.log(arrayNotes[i]);
-                //         console.log(returnAmount);
-                //         document.getElementById("notes").innerHTML = arrayNotes[i] + noteCount++;
-                //         }
-                        
-                //     }
-                // }
-            
+                // loop through array to find notes
+                for (let i = 0; i < arrayNotes.length; i++) {
 
-                for(let i=0;i<arrayNotes.length;i++){
-                   
-                    if(returnAmount >= arrayNotes[i]){
-                        
+                    //check change amount is greater than array value
+                    if (returnAmount >= arrayNotes[i]) {
+
                         let arrayValue = arrayNotes[i];
-                        console.log(arrayValue);
-                        
-                        let remainder = Math.floor(returnAmount / arrayValue) ;
-                        console.log(remainder);
 
-                        var notesAndCount=document.createElement("H4");
-                var message=document.createTextNode(arrayValue + "  "+ "|" + " "+remainder );
-                notesAndCount.appendChild(message);
-                document.body.appendChild(notesAndCount);
+                        let remainder = Math.floor(returnAmount / arrayValue);
 
+                        var notesAndCount = document.createElement("H4");
+                        var message = document.createTextNode(arrayValue + "  " + "|" + " " + remainder);
+                        notesAndCount.appendChild(message);
+                        document.body.appendChild(notesAndCount);
 
-                        for(returnAmount;returnAmount >= arrayNotes[i];){
-                             returnAmount = returnAmount - arrayNotes[i];
+                        // decrement change amount by subtracting array value
+                        for (returnAmount; returnAmount >= arrayNotes[i];) {
+                            returnAmount = returnAmount - arrayNotes[i];
                         }
                     }
 
                 }
-
-            }
-            else{
-                console.log("test fail");
-                var newHeading=document.createElement("H3");
-                var message=document.createTextNode("Cash amount is less than Bill amount !");
-                newHeading.appendChild(message);
-                document.body.appendChild(newHeading);
             }
         })
-    }
-    else{
-        console.log("test fail");
-        var newHeading=document.createElement("H3");
-        var message=document.createTextNode("Enter valid bill amount !");
+    } else {
+        var newHeading = document.createElement("H3");
+        var message = document.createTextNode("Enter valid bill amount !");
         newHeading.appendChild(message);
         document.body.appendChild(newHeading);
     }
+    return inputBill;
 })
-
